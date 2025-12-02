9e81d1714e8cfdc7b5040b08762d553417638169
anand-therattil <anandtherattil@gmail.com>
2025-12-02 14:45:34 +0530

    Fixed the config issue

--- config/loader.py
+++ config/loader.py
@@ -1,7 +1,7 @@
 import yaml
 import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent 
 CONFIG_PATH = os.path.join(BASE_DIR, "config.yaml")
 
 def load_config(path=CONFIG_PATH):
